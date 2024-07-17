8a59975eecdab0cd63989b27a2cee8fab79da2d0
Harn1sh <41642384+Harn1sH@users.noreply.github.com>
2024-07-17 23:31:57 +0530

    added react router

---
+++ client/src/components/navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <div className={'p-3 flex justify-between items-center bg-blue-600 text-white '}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={"1.5"}  stroke="white"
+                 className="size-6">
+                <path strokeLinecap={"round"} strokeLinejoin={"round"}
+                      d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"/>
+            </svg>
+
+            <div className={'flex gap-x-10'}>
+                <button className={'text-white rounded-xl py-1 px-2 active:bg-blue-700 duration-200'}>Login</button>
+                <button className={'text-blue-600 bg-white py-1 px-2 rounded-xl active:bg-stone-200 duration-200'}>Sign-up</button>
+            </div>
+
+        </div>
+    );
+}
+
+export default Navbar;
