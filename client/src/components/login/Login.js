16bfcfcffac81bc0074e758967642212211f0c45
Harn1sh <41642384+Harn1sH@users.noreply.github.com>
2024-07-18 13:14:26 +0530

    made responsive

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,27 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div className={"flex justify-center items-center grow "}>
       <form
         className={
-          "border-2 w-4/12  flex flex-col gap-y-5 border-blue-600 p-7 rounded-lg"
+          "border-2 w:6/12 md:w-4/12  flex flex-col gap-y-5 border-blue-600 p-7 rounded-lg"
         }
       >
         <span className={"text-center text-blue-600 text-3xl font-bold"}>
           Login
         </span>
         <input
-          type="text"
+          type="email"
           className={"border py-1 px-2 rounded-xl"}
           placeholder={"you@mail.com"}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <input
           type="password"
           className={"border py-1 px-2 rounded-xl"}
           placeholder={"password"}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
+
         <button
           className={
             "border-2 transition-all duration-200 hover:bg-blue-600 hover:text-white border-blue-600 py-1 px-2 rounded-xl text-blue-600"
@@ -29,13 +36,44 @@ function Login() {
         >
           Login
         </button>
-        <div className={"flex justify-center space-x-2"}>
-          <span>Dont have an account yet?</span>
-          <Link to="/signup">
-            <span className={"font-semibold text-blue-600 underline"}>
-              Register
-            </span>
-          </Link>
+        <span className={"flex text-gray-500 justify-center"}>or</span>
+        <button className="">
+          <span className={"flex justify-center gap-x-3 items-center w-full"}>
+            <svg
+              className={"h-7"}
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="xMidYMid"
+              viewBox="0 0 256 262"
+            >
+              <path
+                fill="#4285F4"
+                d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"
+              ></path>
+              <path
+                fill="#34A853"
+                d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1"
+              ></path>
+              <path
+                fill="#FBBC05"
+                d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82 0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602l42.356-32.782"
+              ></path>
+              <path
+                fill="#EB4335"
+                d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0 79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"
+              ></path>
+            </svg>
+            Continue with Google
+          </span>
+        </button>
+        <div className={"grid grid-cols-3 mx-auto gap-x-2"}>
+          <span className={"col-span-3 "}>
+            Dont have an account yet?
+            <Link to="/signup">
+              <span className={"font-semibold text-blue-600 underline"}>
+                Register
+              </span>
+            </Link>
+          </span>
         </div>
       </form>
     </div>
