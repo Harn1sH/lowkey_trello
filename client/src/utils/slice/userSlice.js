8920d9e7d905fb037e6d160c80b1f556e23e1ddf
Harn1sh <41642384+Harn1sH@users.noreply.github.com>
2024-07-18 21:55:04 +0530

    configured redux store

---
+++ client/src/utils/slice/userSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    name: null,
+    email: null,
+    id: null,
+  },
+  reducers: {
+    addUser: (state, action) => {
+      state.name = action.payload.firstName;
+      state.email = action.payload.email;
+      state._id = action.payload._id;
+    },
+    removeUser: (state, action) => {
+      state.name = null;
+      state.email = null;
+      state._id = null;
+    },
+  },
+});
+
+export default userSlice.reducer;
+export const { addUser, removeUser } = userSlice.actions;
